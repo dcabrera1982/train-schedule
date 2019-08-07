0467072deca7910a349697567402a187d2dad7ee
David Cabrera <dcabrera1982@gmail.com>
2019-08-06 20:45:12 -0400

    basic html setup

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD_3J-2tI6ku5N6ZJ75GYvjOEs_EqUX5YY",
+    authDomain: "test-project-965f1.firebaseapp.com",
+    databaseURL: "https://test-project-965f1.firebaseio.com",
+    projectId: "test-project-965f1",
+    storageBucket: "test-project-965f1.appspot.com",
+    messagingSenderId: "413722949682",
+    appId: "1:413722949682:web:0ba7e6a4bf7809fd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
